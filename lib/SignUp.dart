8638e0bf2d04cec473d69a3c1b13cdead63e06bc
Rutvij Dave <rutvij.bda1801@ict.gnu.ac.in>
2021-02-27 10:21:35 +0530

    Updates for failing Github Actions Workflows

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'HomePage.dart';
 import 'package:authentification/Login.dart';
 
 class SignUp extends StatefulWidget {
