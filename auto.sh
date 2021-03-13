93f04d77794daaca77f8cf45d7189c3bff49b52a
Rutvij Dave <rutvij.bda1801@ict.gnu.ac.in>
2021-03-13 16:03:55 +0530

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,94 @@
+sleep 5
+adb shell input tap 324 1772
+sleep 3 
+adb shell input tap 196 1306
+sleep 3 
+adb shell input text 'rutvijdave@gmail.com'
+sleep 3 
+adb shell input tap 992 1924
+sleep 3 
+adb shell input tap 137 1433
+sleep 3 
+adb shell input text '11102993'
+sleep 3 
+adb shell input tap 992 1949
+sleep 3 
+adb shell input tap 540 1591
+sleep 3 
+adb shell input tap 560 1728
+sleep 3 
+adb shell input tap 530 1679
+sleep 3 
+adb shell input tap 412 1001
+sleep 3
+adb shell input tap 270 1360
+sleep 3
+adb shell input text 'rutvijdave@scb'
+sleep 3
+adb shell input tap 997 1944
+sleep 3
+adb shell input tap 545 1782
+sleep 3
+adb shell input tap 187 1424
+sleep 3
+adb shell input text 'your Transaction Code'
+sleep 3
+adb shell input tap 972 1949
+sleep 3
+adb shell input tap 520 1846
+sleep 3
+adb shell input tap 520 1846
+sleep 3
+adb shell input tap 545 1969
+sleep 3
+adb shell input tap 515 1699
+sleep 5
+adb shell input tap 712 1777
+sleep 3
+adb shell input tap 137 1296
+sleep 3
+adb shell input text 'rutvijdave'
+sleep 3
+adb shell input tap 992 1949
+sleep 3
+adb shell input tap 147 1448
+sleep 3
+adb shell input text 'rutvijdave1212@gmail.com'
+sleep 3
+adb shell input tap 992 1949
+sleep 3
+adb shell input tap 137 1591
+sleep 3
+adb shell input text '11102993'
+sleep 3
+adb shell input tap 992 1949
+sleep 3
+adb shell input tap 535 1748
+sleep 3
+adb shell input tap 560 1728
+sleep 3
+adb shell input tap 530 1679
+sleep 3
+adb shell input tap 412 1001
+sleep 3
+adb shell input tap 270 1360
+sleep 3
+adb shell input text 'rutvijdave@scb'
+sleep 3
+adb shell input tap 997 1944
+sleep 3
+adb shell input tap 545 1782
+sleep 3
+adb shell input tap 187 1424
+sleep 3
+adb shell input text 'your Transaction Code'
+sleep 3
+adb shell input tap 972 1949
+sleep 3
+adb shell input tap 520 1846
+sleep 3
+adb shell input tap 520 1846
+sleep 3
+adb shell input tap 545 1969
+sleep 3
+adb shell input tap 515 1699
