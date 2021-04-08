8df527fe49701cc7110fbdfb556165e09c3dee9f
Rutvij Dave <rutvij.bda1801@ict.gnu.ac.in>
2021-04-08 11:02:33 +0530

    new scripts

---
+++ build1.sh
@@ -0,0 +1,46 @@
+#Define all paths, constants here
+PROJECT_DIR='C:/Users/Rutvij/Documents/Digital Public Transport/'
+OUTPUT_DIR='C:/Users/Rutvij/Documents/Digital Public Transport/build/app/outputs/'
+# Functions for customising print colors (Optional)
+print_red(){
+    printf "\e[1;31m$1\e[0m"
+}
+print_green(){
+    printf "\e[1;32m$1\e[0m"
+}
+print_yellow(){
+    printf "\e[1;33m$1\e[0m"
+}
+print_blue(){
+    printf "\e[1;34m$1\e[0m"
+}
+#Start Build Process
+print_blue "\n\n\nStarting Build Process"
+wait
+print_blue "\n\n\nChecking Dependencies"
+flutter doctor -v
+wait
+print_blue "\n\n\nGet Packages"
+flutter pub get
+wait
+print_blue "\n\n\nBuilding now...\n"
+flutter build apk --split-per-abi --no-shrink
+wait
+print_blue "\n\n\n Build Created...\n"
+#Install APK on device / emulator
+print_blue "\n\n\nInstalling APK on emulator...\n"
+flutter install app-armeabi-v7a-release.apk
+print_blue "Done Installing\n"
+print_blue "\n\n\n Launching Now..\n"
+wait
+#Launch Main Activity
+adb shell am start -n "com.example.authentification/com.example.authentification.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+print_blue "\n\n\n Launched main activity\n"
+print_green "\n\n\nNow Lets Start the test\n"
+sleep 60
+#sh ./emu.sh
+#sleep 120
+sh ./auto1.sh
+wait
+
+exit
